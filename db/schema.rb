e0a734e2c7330ac2b582541a3a27978463c22cb9
sagarpednekar <sagarpednekar04@gmail.com>
2016-07-29 17:06:16 +0530

    Add styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729102235) do
+ActiveRecord::Schema.define(version: 20160729113023) do
+
+  create_table "resumes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "user"
